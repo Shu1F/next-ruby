95146df9613a7f72f812e4479a5d06ace6dd0823
fs203 <fs2031@icloud.com>
2025-05-08 22:19:17 +0900

    add : styles

--- blog_client/src/app/page.tsx
+++ blog_client/src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import styles from "@/styles/Home.module.css";
+import { Post } from "@/types";
+
+type Props = {
+  posts: Post[];
+};
 
 // app/posts/page.tsx
 export default async function PostsPage() {
@@ -10,11 +16,19 @@ export default async function PostsPage() {
   const posts = await res.json();
 
   return (
-    <div>
-      {posts.map((p: any) => (
-        <div key={p.id}>
-          <div>{p.title}</div>
-          <div>{p.content}</div>
+    <div className={styles.homeContainer}>
+      <h2>Rails & Next.js Blog</h2>
+      <Link href="/create-post" className={styles.createButton}>
+        Create new Post
+      </Link>
+      {posts.map((post: Post) => (
+        <div key={post.id} className={styles.postCard}>
+          <Link href={`posts/${post.id}`} className={styles.postCardBox}>
+            <h2>{post.title}</h2>
+          </Link>
+          <p>{post.content}</p>
+          <button className={styles.editButton}>Edit</button>
+          <button className={styles.deleteButton}>Delete</button>
         </div>
       ))}
     </div>
